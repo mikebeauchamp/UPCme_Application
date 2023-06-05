c08f9741440a2f724be7a725ec3aef0dff583c6d
Michael Beauchamp <beauchampmichaele@gmail.com>
2023-06-04 20:32:02 -0400

    2023.06.04: Added camera permission logic to MainActivity.java

--- app/src/main/java/com/example/sweng894_capstone_upcme/MainActivity.java
+++ app/src/main/java/com/example/sweng894_capstone_upcme/MainActivity.java
@@ -3,34 +3,53 @@ package com.example.sweng894_capstone_upcme;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.budiyev.android.codescanner.AutoFocusMode;
+
 import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
-import com.budiyev.android.codescanner.ErrorCallback;
-import com.budiyev.android.codescanner.ScanMode;
-import com.google.zxing.Result;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    private CodeScanner codeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setupPermissions();
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void setupPermissions(){
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest();
+        }
+    }
+
+    private void makeRequest(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults.length == 0) {
+                Toast.makeText(this, "Camera permissions are required to run this application.", Toast.LENGTH_SHORT).show();
+            } else {
+                //Permissions given
+            }
+        }
     }
 }
